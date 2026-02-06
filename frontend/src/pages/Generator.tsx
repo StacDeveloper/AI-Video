287f9fa29631a1b051a4763226fff01a04a0a54e
Soham Mule <sohammule09@gmail.com>
2026-02-06 13:30:59 +0000

    First-Commit

---
+++ frontend/src/pages/Generator.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const Generator = () => {
+  return (
+    <div className='min-h-screen text-white p-6 md:p-12 mt-28'>
+        <form>
+            <Title heading='Create In-Context Image'/>
+        </form>
+    </div>
+  )
+}
+
+export default Generator
