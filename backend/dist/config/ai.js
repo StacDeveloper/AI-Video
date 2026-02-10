2402ea164bc837de4c5a2a9814863f219b767f3e
Soham Mule <sohammule09@gmail.com>
2026-02-10 14:28:01 +0000

    dist-changes

--- backend/dist/config/ai.js
+++
@@ -1,5 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-const ai = new GoogleGenAI({
-    apiKey: process.env.GOOGLE_GEMINI_API
-});
-export default ai;
--- backend/dist/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { protect } from "../middleware/auth.js";
-import { getAllProject, getProjectbyId, getUserCredits, toggleProjectPublic } from "../controllers/usercontroller.js";
-const userRouter = express.Router();
-userRouter.get("/credits", protect, getUserCredits);
-userRouter.get("/projects", protect, getAllProject);
-userRouter.get("/projects/:projectId", protect, getProjectbyId);
-userRouter.get("/publish/:projectId", protect, toggleProjectPublic);
-export default userRouter;
